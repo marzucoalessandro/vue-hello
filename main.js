209740c2da118525f5d8ae8e75753aa9bfa3c85c
Alessandro <marzucoalessandro1@gmail.com>
2020-12-14 15:17:27 +0100

    inizio

---
+++ main.js
@@ -0,0 +1,11 @@
+// Istruzioni:
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Bonus: Aggiungere un’immagine presa anch’essa da un data
+
+//1 istanziamo Vue;
+let app = new Vue({
+  el: "#root",
+  data: {
+    text: "prova con vue"
+  }
+})
